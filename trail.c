e4a414e229f289132b970127537232da98be8eb9
CSE <ayush092003@gmail.com>
2024-08-01 11:25:20 +0530

    Commit

--- trail.c
+++ trail.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int main(){
+<<<<<<< HEAD
 	printf("This is Second Time : Hello World");
+=======
+	printf(" Hello World");
+>>>>>>> parent of 0eb8659 (Revert "Trying to commit first time")
 	return 0;
 }
